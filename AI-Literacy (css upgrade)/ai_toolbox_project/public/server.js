6f1d353be678ea8aa5cfd7cd13c55b46b78e661d
Kevin <kevinflotow@arcor.de>
2025-06-29 17:32:26 +0200

    some cleanup and exported the db-schema

--- AI-Literacy (css upgrade)/ai_toolbox_project/public/server.js
+++ AI-Literacy (css upgrade)/ai_toolbox_project/public/server.js
@@ -1,10 +1,17 @@
 // server.js
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Tool = require('./models/dbTools.js');
+const dotenv = require('dotenv');
 
 // Nur Mongoose verwenden
 const mongoose = require('mongoose');
+
 const app = express();
+// um Daten aus der .env-Datei zu erhalten
+dotenv.config();
+
 
 // logger importieren
 const logger = require('./logger');
@@ -24,60 +31,6 @@ app.use(cors());
 // JSON parsing middleware
 app.use(express.json());
 
-// UnterSchema für ratings
-  const ratingSchema = new mongoose.Schema({
-  totalRatings: {
-    type: [Number],
-    default: [],
-    validate: {
-      validator: function(arr) {
-        // testet ob jeder Wert im Array auch Teil des vordefinierten Arrays ist
-        return arr.every(val => [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0].includes(val));
-      },
-      // schlägt die Validierung fehl wird automatisch die message ausgegeben
-      message: "Rating Typ ist nicht benutzbar: {VALUE}"
-    }
-  },
-  averageRating: { type: Number, default: 0.0 },
-  ratingCount: { type: Number, default: 0 }
-});
-
-// Definiere das Schema -> definiert die genaue Struktur der Daten mit Datentypen
-const userUploadSchema = new mongoose.Schema({
-    author : { type:String ,
-               required: true },
-    uploaderEmail: { type: String ,
-                     required: true ,
-                     match:[ /.+\@.+\..+/ ,
-                    "Please enter a valid E-Mail adress"]},          
-    uploadType:    { type: String,
-                     required: true }, 
-    uploadDate:    { type: Date,
-                     default: new Date()},          // Date Objekt muss noch formatiert werden
-    ageRecommendation: { type: Number,
-                         required: true },
-    uploadTitle:   { type: String ,
-                     required: true},
-    uploadDescription:{ type: String ,
-                        required:true},
-    fileURL:{type: String ,
-             required:true},
-    thumbnailURL: { type : String ,
-                    required:true},
-    uploadTags : {type : [String] ,
-                  default: []},
-    reviweStatus : { type : String ,
-                     default: "pending"},
-    reviewNotes : { type : String },
-    reviewedByAdmin: { type: String ,
-                       default: null},
-    rating : { type: ratingSchema }
-  });
-
-// Modell definieren -> stellt eine Blaupause zu Verfügung 
-// nimmt den Namen der Collection und das vordefinierte Schema entgegen
-const uploadModel = mongoose.model('userUploads' , userUploadSchema);
-
 // Server Test
 app.get('/', (req, res) => {
   res.send('Server ist live!');
@@ -88,7 +41,7 @@ app.get('/get-data' , async (req , res) => {
 
     try{
         // mit find() werden alle Dokumente aus der Collection abgerufen -> gibt Array aus Objekten(Dokumenten) zurück
-        const data = await uploadModel.find();
+        const data = await Tool.find();
         res.json(data);
     } catch (error){
         logger.error(`Fehler im Get Handler : ${err.message} `);
@@ -126,7 +79,7 @@ app.post('/add-entry', async (req, res) => {
 
         res.status(200).send('Daten erfolgreich gespeichert!');
     } catch (error) {
-        logger.error(`Fehler beim Speichern der Daten : ${err.message`);
+        logger.error(`Fehler beim Speichern der Daten : ${err.message}`);
         res.status(500).send('Interner Serverfehler.');
     }
 });
